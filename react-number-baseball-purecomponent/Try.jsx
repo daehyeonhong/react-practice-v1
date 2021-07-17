e9612695345d05889b031ac9994dd9e72ca44282
DaeHyeonHong <hongdae@zen9ss.co.kr>
2021-07-17 23:14:36 +0900

    try

--- react-number-baseball-purecomponent/Try.jsx
+++ react-number-baseball-purecomponent/Try.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {memo} from 'react';
 
-const Try = ({tryInfo}) => {
+//class
+/*class Try extends PureComponent {
+    render() {
+        const {tryInfo} = this.props;
+        return (<li>
+                <div>{tryInfo.try}</div>
+                <div>{tryInfo.result}</div>
+            </li>
+        );
+    }
+}*/
+
+//hooks
+
+const Try = memo(({tryInfo}) => {
+    console.log(`메렁`);
     return (
         <li>
             <div>{tryInfo.try}</div>
             <div>{tryInfo.result}</div>
         </li>
     );
-};
+});
 
 export default Try;
